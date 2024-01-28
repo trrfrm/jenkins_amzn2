7b7bd56eea8f8f68bc2ed16498f1bbd02894090d
nykzaR <nykzar@outlook.com>
2024-01-28 21:06:34 +0530

    added jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install jenkins
+sudo su -
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+# Install git
+sudo yum install git -y
+
+# Install Terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
